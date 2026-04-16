8a8e618f081750edda6bc31619f1cf6db28441d7
Nicolas Perez <nicoperez.dev@gmail.com>
2026-04-16 14:56:35 -0400

    Ejercicio 20: Título: sunt aut facere repellat provident occaecati excepturi optio reprehenderit

--- python-desarrollo/ejercicio20.py
+++ python-desarrollo/ejercicio20.py
@@ -9,7 +9,13 @@ import requests
 #    "Error al obtener el post"
 
 def obtener_post():
-    # TODO: tu código aquí
-    pass
+   try:
+        response = requests.get("https://jsonplaceholder.typicode.com/posts/1")
+        
+        if response.status_code == 200:
+            data = response.json()
+            print(f"Título: {data['title']}")
+    except:
+        print("Error al obtener el post")
 
 obtener_post()
